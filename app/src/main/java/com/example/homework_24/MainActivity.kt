0b69a2b96001d3075b7544e794e258a20539f723
MaksimMaksimchyk <maksim.maksimchyk@yandex.by>
2025-12-01 01:09:22 +0300

    Done

--- app/src/main/java/com/example/homework_24/MainActivity.kt
+++ app/src/main/java/com/example/homework_24/MainActivity.kt
@@ -1,20 +1,67 @@
 package com.example.homework_24
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.viewModelScope
+import com.example.homework_24.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.contracts.Effect
+import kotlin.getValue
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupObservers()
+        setupListeners()
     }
+
+    private fun setupObservers() {
+
+        viewModel.result.observe(this) {
+            binding.resultView.text = it.toString()
+            hideProgressBar()
+        }
+
+    }
+
+    private fun setupListeners() {
+
+        binding.loadButton.setOnClickListener {
+            showProgressBar()
+            viewModel.getResult()
+        }
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+
 }
+
+
